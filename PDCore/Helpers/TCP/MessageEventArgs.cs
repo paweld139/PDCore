841b7fc4ada3bbf499be1ce600ad7479c8d21b73
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-07-29 21:27:31 +0200

    Fix

---
+++ PDCore/Helpers/TCP/MessageEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PDCore.Helpers.TCP
+{
+    public class MessageEventArgs : EventArgs
+    {
+        public string Text { get; private set; }
+
+        public MessageEventArgs(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ PDCore/Helpers/WPF/CaliburnMicro/IDialogManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Caliburn.Micro;
+using PDCore.Enums;
+using System.Windows;
+using MessageBoxOptions = PDCore.Enums.MessageBoxOptions;
+
+namespace PDCore.Helpers.WPF.CaliburnMicro
+{
+    public interface IDialogManager
+    {
+        void ShowDialog(IScreen dialogModel, VerticalAlignment verticalAlignment = VerticalAlignment.Center,
+            HorizontalAlignment horizontalAlignment = HorizontalAlignment.Center);
+        void ShowMessageBox(string message, string title = null, MessageBoxOptions options = MessageBoxOptions.Ok,
+            Action<IMessageBox> callback = null, VerticalAlignment verticalAlignment = VerticalAlignment.Center,
+            HorizontalAlignment horizontalAlignment = HorizontalAlignment.Center);
+    }
+}
