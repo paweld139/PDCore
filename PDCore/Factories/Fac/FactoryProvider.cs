e887acb1b85e992ca9581fa2291a402c22c0f9b0
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-07-31 15:27:53 +0200

    Fix

---
+++ PDCore/Factories/Fac/FactoryProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace PDCore.Factories.Fac
+{
+    public abstract class FactoryProvider<TFactory, TIFactory>
+    {
+        private static IEnumerable<Type> factories;
+
+        private static bool IsInitialized => factories != null;
+
+        public FactoryProvider()
+        {
+            if (!IsInitialized)
+                InitializeFactories();
+        }
+
+        private void InitializeFactories()
+        {
+            factories = Assembly.GetAssembly(typeof(TFactory))
+                   .GetTypes()
+                   .Where(t => typeof(TIFactory).IsAssignableFrom(t));
+        }
+
+        public virtual TIFactory CreateFactoryFor(string name)
+        {
+            var factory = factories.Single(x =>
+                x.Name.ToLowerInvariant().Contains(name.ToLowerInvariant()));
+
+            return (TIFactory)Activator.CreateInstance(factory);
+        }
+
+        public abstract TIFactory CreateFactoryFor(params object[] parameters);
+    }
+}
---
+++ PDCore/Lazy/ValueHolder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PDCore.Lazy
+{
+    public interface IValueHolder<T>
+    {
+        T GetValue(object parameter);
+    }
+
+    public class ValueHolder<T> : IValueHolder<T>
+    {
+        private readonly Func<object, T> getValue;
+        private T value;
+
+        public ValueHolder(Func<object, T> getValue)
+        {
+            this.getValue = getValue;
+        }
+
+        public T GetValue(object parameter)
+        {
+            if (value == null)
+            {
+                value = getValue(parameter);
+            }
+
+            return value;
+        }
+    }
+}
