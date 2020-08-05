73141c42d56ca2ca51b6714128f51a4475d77766
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-08-05 05:24:26 +0200

    Development

--- PDWebCore/Scripts/Tools.js
+++ PDWebCore/Scripts/Tools.js
@@ -701,3 +701,13 @@ function GetUTCOffset() {
     var hrs = -(new Date().getTimezoneOffset() / 60);
     return hrs;
 }
+
+function parseISOString(s) {
+    var b = s.split(/\D+/);
+    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+}
+
+function isoFormatDMY(d) {
+    function pad(n) { return (n < 10 ? '0' : '') + n }
+    return pad(d.getUTCDate()) + '/' + pad(d.getUTCMonth() + 1) + '/' + d.getUTCFullYear();
+}
