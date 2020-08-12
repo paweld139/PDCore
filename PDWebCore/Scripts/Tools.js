534ed5a1fe5e04404341f4ebf72083ebfa81a684
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-08-12 22:27:37 +0200

    Fix

--- PDWebCore/Scripts/Tools.js
+++ PDWebCore/Scripts/Tools.js
@@ -606,6 +606,20 @@ ko.bindingHandlers.fadeVisible = {
     }
 };
 
+ko.bindingHandlers.fadeVisibleWithDuration = {
+    init: function (element, valueAccessor) {
+        var shouldDisplay = valueAccessor();
+        $(element).toggle(shouldDisplay);
+    },
+    update: function (element, valueAccessor, allBindingsAccessor) {
+        var shouldDisplay = valueAccessor(),
+            allBindings = allBindingsAccessor(),
+            duration = allBindings.fadeDuration || 500;
+
+        shouldDisplay ? $(element).fadeIn(duration) : $(element).fadeOut(duration);
+    }
+};
+
 ko.bindingHandlers.starRating = {
     init: function (element, valueAccessor) {
         $(element).addClass("starRating");
