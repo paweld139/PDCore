ed20b89a5100bacfb02aaedab0d5aa297e61eb3a
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-09-01 20:36:14 +0200

    Fix

--- PDCore/Utils/SecurityUtils.cs
+++ PDCore/Utils/SecurityUtils.cs
@@ -271,5 +271,17 @@ namespace PDCore.Utils
 
             return hashString;
         }
+
+        public static string Sha512(string input)
+        {
+
+            using (var sha = SHA512.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(input);
+                var hash = sha.ComputeHash(bytes);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
     }
 }
